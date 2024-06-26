0bfaf77d795f8a171d325c1b877242aeb654385d
ikhsanurasidb <muhammad.nurasid@binus.ac.id>
2024-06-26 23:35:46 +0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Muhamamd Nurasid on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
